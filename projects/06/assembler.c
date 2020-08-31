12aa48f7a4146ea46667c2c13a12f14116c5bc7e
agrim334 <agrim334@gmail.com>
2020-08-31 23:59:22 +0530

    add parsing

--- projects/06/assembler.c
+++ projects/06/assembler.c
@@ -1,166 +1,20 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include"lexer.h"
+char curcom[100];
 
-char instruct[17];
-int destb[3];
-int compb[7];
-int jumpb[3];
-
-int constant(char ar[],int* ptr){
-	int i = 0;
-	int value = 0;
-	while(ar[*ptr]!='\n' && ar[*ptr] >= '0' && ar[*ptr] <= '9'){
-		value=value*10+ar[*ptr]-'0';
-		*ptr=*ptr+1;
-	}
-
-	return value;
+int hasMoreCommands(FILE* fp){
+	return !feof(fp)?1:0;
 }
-void ST(char ar[]){
-	;
+void advance(FILE** fp){
+	fgets(curcom,100,*fp);
 }
-
-int comp(char ar[]){
-	if(ar[0] == '0'){
-		compb[0]=0;
-		compb[1]=1;
-		compb[2]=0;
-		compb[3]=1;
-		compb[4]=0;
-		compb[5]=1;
-		compb[6]=0;
-	}
-	else if(ar[0] == '1'){
-		compb[0]=0;
-		compb[1]=1;
-		compb[2]=1;
-		compb[3]=1;
-		compb[4]=1;
-		compb[5]=1;
-		compb[6]=1;
-	}
-	else if(ar[0] == '-' && ar[1] == '1'){
-		compb[0]=0;
-		compb[1]=1;
-		compb[2]=1;
-		compb[3]=1;
-		compb[4]=0;
-		compb[5]=1;
-		compb[6]=0;
-	}
-	else if(ar[0] == '-' && ar[1] == '1'){
-		compb[0]=0;
-		compb[1]=1;
-		compb[2]=1;
-		compb[3]=1;
-		compb[4]=0;
-		compb[5]=1;
-		compb[6]=0;
-	}
-
+void commandType(){
+;
 }
-
-int dest(char ar[]){
-	if(ar[*ptr] == 'M' && ar[*ptr+1] != 'D'){
-		destb[0]=0;
-		destb[1]=0;
-		destb[2]=1;
-	}
-	else if(ar[*ptr] == 'D'){
-		destb[0]=0;
-		destb[1]=1;
-		destb[2]=0;
-	}
-	else if(ar[*ptr] == 'M' && ar[*ptr+1] == 'D'){
-		destb[0]=0;
-		destb[1]=1;
-		destb[2]=1;
-	}
-	else if(ar[*ptr] == 'A' && ar[*ptr+1] == 'M' && ar[*ptr+2] == 'D'){
-		destb[0]=1;
-		destb[1]=1;
-		destb[2]=1;
-	}
-	else if(ar[*ptr] == 'A' && ar[*ptr+1] == 'M'){
-		destb[0]=1;
-		destb[1]=0;
-		destb[2]=1;
-	}
-	else if(ar[*ptr] == 'A' && ar[*ptr+1] == 'D'){
-		destb[0]=1;
-		destb[1]=1;
-		destb[2]=0;
-	}
-	else if(ar[*ptr] == 'A'){
-		destb[0]=1;
-		destb[1]=0;
-		destb[2]=0;
-	}
-}
-
-void jump(char ar[],int* ptr){
-	if(ar[*ptr]=='J'){
-		if(ar[*ptr+1] == 'G' && ar[*ptr+2] == 'T'){
-			jumpb[0]=0;
-			jumpb[1]=0;
-			jumpb[2]=1;
-		}
-		else if(ar[*ptr+1] == 'E' && ar[*ptr+2] == 'Q'){
-			jumpb[0]=0;
-			jumpb[1]=1;
-			jumpb[2]=0;
-		}
-		else if(ar[*ptr+1] == 'G' && ar[*ptr+2] == 'E'){
-			jumpb[0]=0;
-			jumpb[1]=1;
-			jumpb[2]=1;
-		}
-		else if(ar[*ptr+1] == 'L' && ar[*ptr+2] == 'T'){
-			jumpb[0]=1;
-			jumpb[1]=0;
-			jumpb[2]=0;
-		}
-		else if(ar[*ptr+1] == 'N' && ar[*ptr+2] == 'E'){
-			jumpb[0]=1;
-			jumpb[1]=0;
-			jumpb[2]=1;
-		}
-		else if(ar[*ptr+1] == 'L' && ar[*ptr+2] == 'E'){
-			jumpb[0]=1;
-			jumpb[1]=1;
-			jumpb[2]=0;
-		}
-		else if(ar[*ptr+1] == 'M' && ar[*ptr+2] == 'P'){
-			jumpb[0]=1;
-			jumpb[1]=1;
-			jumpb[2]=1;
-		}
-	}
-}
-
-void Ainstruct(char ar[],int* ptr){
-	int i;
-	instruct[0]='0';
-	instruct[16]='\0';
-
-	*ptr = *ptr+1;
-	int value = constant(ar,ptr);
-	for(i=15;i>0;i--){
-		instruct[i]= (value & 1) + '0';
-		value = value >> 1;
-	}
-}
-
-void whitespace(char ar[],int* ptr){
-	while(ar[*ptr]==' '||ar[*ptr]=='\t'||ar[*ptr]=='\n'){
-		*ptr = *ptr+1;
-	}
-}
-
-void comments(char ar[],int* ptr){
-	while(ar[*ptr]!='\n')
-		*ptr = *ptr+1;
+void symbol(){
+;
 }
 
 int main(int argc, char* argv[]){
@@ -182,28 +36,34 @@ int main(int argc, char* argv[]){
 	ar[i+4]='k';
 	ar[i+5]='\0';
 	FILE* hackf= fopen(ar,"w+");
-	char temp[100];
-	fgets(temp,100,asmf);
 	int ptr=0;
-
-	while(!feof(asmf)){
-		whitespace(temp,&ptr);
-		int x = strlen(temp);
-		printf("%s",temp);
+	int x = 0;
+	while(hasMoreCommands(asmf)){
+		ptr=0;
+		advance(&asmf);
+		whitespace(curcom,&ptr);
+		x = 0;
+		while(curcom[x]!='\n')
+			x++;
+		printf("%s",curcom);
 		if(ptr < x) {
-			if(temp[ptr]=='/' && ptr+1 < x && temp[ptr+1]=='/'){
-				comments(temp,&ptr);
+			if(curcom[ptr]=='/' && ptr+1 < x && curcom[ptr+1]=='/'){
+				comments(curcom,&ptr);
 			}
-			if(ptr < x && temp[ptr]!='\0'&&temp[ptr]!='\n'){
-				if(temp[ptr]=='@') {
-					printf("%s\n",Ainstruct(temp,&ptr));
+			else if(ptr < x && curcom[ptr] != '\0' && curcom[ptr] != '\n' && curcom[ptr] != ' ' && curcom[ptr] != '\t'){
+				if(curcom[ptr]=='@') {
+					Ainstruct(curcom,&ptr);
 				}
-				else if(temp[ptr]>='0' && temp[ptr]<='9')
-					printf("%d\n",constant(temp,&ptr));
+				else if (curcom[ptr]!='@'){
+					Cinstruct(curcom,&ptr);
+				}
+				printf("%s\n",instruct);
+				for(int i=0;i<16;i++)
+					instruct[i]='0';
+				instruct[16]='\0';
+
 			}
 		}
-		ptr=0;
-		fgets(temp,100,asmf);
 	}
 	fclose(hackf);
 	fclose(asmf);
---
+++ projects/06/lexer.h
@@ -0,0 +1,493 @@
+char instruct[17];
+char destb[4];
+char jumpb[4];
+char compb[8];
+
+int constant(char ar[],int* ptr){
+	int i = 0;
+	int value = 0;
+	while(ar[*ptr]!='\n' && ar[*ptr] >= '0' && ar[*ptr] <= '9'){
+		value=value*10+ar[*ptr]-'0';
+		*ptr=*ptr+1;
+	}
+
+	return value;
+}
+void ST(char ar[]){
+	;
+}
+
+char* comp(char ar[],int* ptr){
+	switch(ar[*ptr]){
+		case '0':
+			compb[0]='0';
+			compb[1]='1';
+			compb[2]='0';
+			compb[3]='1';
+			compb[4]='0';
+			compb[5]='1';
+			compb[6]='0';
+		break;
+		case '1':
+			compb[0]='0';
+			compb[1]='1';
+			compb[2]='1';
+			compb[3]='1';
+			compb[4]='1';
+			compb[5]='1';
+			compb[6]='1';
+		break;
+		case '-':
+			switch(ar[*ptr+1]){
+				case '1':
+					compb[0]='0';
+					compb[1]='1';
+					compb[2]='1';
+					compb[3]='1';
+					compb[4]='0';
+					compb[5]='1';
+					compb[6]='0';
+				break;
+				case 'D':
+					compb[0]='0';
+					compb[1]='0';
+					compb[2]='0';
+					compb[3]='1';
+					compb[4]='1';
+					compb[5]='1';
+					compb[6]='1';
+				break;
+
+				case 'A':
+					compb[0]='0';
+					compb[1]='1';
+					compb[2]='1';
+					compb[3]='0';
+					compb[4]='0';
+					compb[5]='1';
+					compb[6]='1';
+				break;
+	
+				case 'M':
+					compb[0]='1';
+					compb[1]='1';
+					compb[2]='1';
+					compb[3]='0';
+					compb[4]='0';
+					compb[5]='1';
+					compb[6]='1';
+				break;
+			}
+		break;
+		case '!':
+			switch(ar[*ptr+1]){
+				case 'D':
+					compb[0]='0';
+					compb[1]='0';
+					compb[2]='0';
+					compb[3]='1';
+					compb[4]='1';
+					compb[5]='0';
+					compb[6]='1';
+				break;
+
+				case 'A':
+					compb[0]='0';
+					compb[1]='1';
+					compb[2]='1';
+					compb[3]='0';
+					compb[4]='0';
+					compb[5]='0';
+					compb[6]='1';
+				break;
+	
+				case 'M':
+					compb[0]='1';
+					compb[1]='1';
+					compb[2]='1';
+					compb[3]='0';
+					compb[4]='0';
+					compb[5]='0';
+					compb[6]='1';
+				break;
+			}
+		break;
+		case 'D':
+			switch(ar[*ptr+1]){
+				case '-':
+					switch(ar[*ptr+2]){
+					case '1':
+						compb[0]='0';
+						compb[1]='0';
+						compb[2]='0';
+						compb[3]='1';
+						compb[4]='1';
+						compb[5]='1';
+						compb[6]='0';
+					break;
+
+					case 'A':
+						compb[0]='0';
+						compb[1]='0';
+						compb[2]='1';
+						compb[3]='0';
+						compb[4]='0';
+						compb[5]='1';
+						compb[6]='1';
+					break;
+					case 'M':
+						compb[0]='1';
+						compb[1]='0';
+						compb[2]='1';
+						compb[3]='0';
+						compb[4]='0';
+						compb[5]='1';
+						compb[6]='1';
+					break;
+					}
+				break;
+				case '+':
+					switch(ar[*ptr+2]){
+					case '1':
+						compb[0]='0';
+						compb[1]='0';
+						compb[2]='1';
+						compb[3]='1';
+						compb[4]='1';
+						compb[5]='1';
+						compb[6]='1';
+					break;
+
+					case 'A':
+						compb[0]='0';
+						compb[1]='0';
+						compb[2]='0';
+						compb[3]='0';
+						compb[4]='0';
+						compb[5]='1';
+						compb[6]='0';
+					break;
+					case 'M':
+						compb[0]='1';
+						compb[1]='0';
+						compb[2]='0';
+						compb[3]='0';
+						compb[4]='0';
+						compb[5]='1';
+						compb[6]='0';
+					break;
+					}
+				break;
+				case '&':
+					compb[1]='0';
+					compb[2]='0';
+					compb[3]='0';
+					compb[4]='0';
+					compb[5]='0';
+					compb[6]='0';
+					if(ar[*ptr+2] == 'A')
+						compb[0]='0';
+					else
+						compb[0]='1';
+					break;
+	
+				case '|':
+					compb[1]='0';
+					compb[2]='1';
+					compb[3]='0';
+					compb[4]='1';
+					compb[5]='0';
+					compb[6]='1';
+					if(ar[*ptr+2] == 'A')
+						compb[0]='0';
+					else
+						compb[0]='1';
+					break;
+	
+				default:
+					compb[0]='0';
+					compb[1]='0';
+					compb[2]='0';
+					compb[3]='1';
+					compb[4]='1';
+					compb[5]='0';
+					compb[6]='0';				
+			}
+		break;
+		case 'A':
+			switch(ar[*ptr+1]){
+				case '+':
+					compb[0]='0';
+					compb[1]='1';
+					compb[2]='1';
+					compb[3]='0';
+					compb[4]='1';
+					compb[5]='1';
+					compb[6]='1';
+				break;
+
+				case '-':
+					switch(ar[*ptr+2]){
+						case '1':
+							compb[0]='0';
+							compb[1]='1';
+							compb[2]='1';
+							compb[3]='0';
+							compb[4]='0';
+							compb[5]='1';
+							compb[6]='0';
+						break;
+
+						case 'D':
+							compb[0]='0';
+							compb[1]='0';
+							compb[2]='0';
+							compb[3]='0';
+							compb[4]='1';
+							compb[5]='1';
+							compb[6]='1';
+						break;
+					}
+				break;
+				default:
+					compb[0]='0';
+					compb[1]='1';
+					compb[2]='1';
+					compb[3]='0';
+					compb[4]='0';
+					compb[5]='0';
+					compb[6]='0';
+			}
+		break;
+		case 'M':
+			switch(ar[*ptr+1]){
+				case '+':
+					compb[0]='1';
+					compb[1]='1';
+					compb[2]='1';
+					compb[3]='0';
+					compb[4]='1';
+					compb[5]='1';
+					compb[6]='1';
+				break;
+
+				case '-':
+					switch(ar[*ptr+2]){
+						case '1':
+							compb[0]='1';
+							compb[1]='1';
+							compb[2]='1';
+							compb[3]='0';
+							compb[4]='0';
+							compb[5]='1';
+							compb[6]='0';
+						break;
+
+						case 'D':
+							compb[0]='1';
+							compb[1]='0';
+							compb[2]='0';
+							compb[3]='0';
+							compb[4]='1';
+							compb[5]='1';
+							compb[6]='1';
+						break;
+					}
+				break;
+				default:
+					compb[0]='1';
+					compb[1]='1';
+					compb[2]='1';
+					compb[3]='0';
+					compb[4]='0';
+					compb[5]='0';
+					compb[6]='0';
+			}
+		break;
+	}
+	return compb;
+}
+
+char* dest(char ar[],int* ptr){
+	if(ar[*ptr] == 'M' && ar[*ptr+1] != 'D'){
+		destb[0]='0';
+		destb[1]='0';
+		destb[2]='1';
+	}
+	else if(ar[*ptr] == 'D'){
+		destb[0]='0';
+		destb[1]='1';
+		destb[2]='0';
+	}
+	else if(ar[*ptr] == 'M' && ar[*ptr+1] == 'D'){
+		destb[0]='0';
+		destb[1]='1';
+		destb[2]='1';
+	}
+	else if(ar[*ptr] == 'A' && ar[*ptr+1] == 'M' && ar[*ptr+2] == 'D'){
+		destb[0]='1';
+		destb[1]='1';
+		destb[2]='1';
+	}
+	else if(ar[*ptr] == 'A' && ar[*ptr+1] == 'M'){
+		destb[0]='1';
+		destb[1]='0';
+		destb[2]='1';
+	}
+	else if(ar[*ptr] == 'A' && ar[*ptr+1] == 'D'){
+		destb[0]='1';
+		destb[1]='1';
+		destb[2]='0';
+	}
+	else if(ar[*ptr] == 'A'){
+		destb[0]='1';
+		destb[1]='0';
+		destb[2]='0';
+	}
+	return destb;
+}
+
+char* jump(char ar[],int* ptr){
+	if(ar[*ptr]=='J'){
+		if(ar[*ptr+1] == 'G' && ar[*ptr+2] == 'T'){
+			jumpb[0]='0';
+			jumpb[1]='0';
+			jumpb[2]='1';
+		}
+		else if(ar[*ptr+1] == 'E' && ar[*ptr+2] == 'Q'){
+			jumpb[0]='0';
+			jumpb[1]='1';
+			jumpb[2]='0';
+		}
+		else if(ar[*ptr+1] == 'G' && ar[*ptr+2] == 'E'){
+			jumpb[0]='0';
+			jumpb[1]='1';
+			jumpb[2]='1';
+		}
+		else if(ar[*ptr+1] == 'L' && ar[*ptr+2] == 'T'){
+			jumpb[0]='1';
+			jumpb[1]='0';
+			jumpb[2]='0';
+		}
+		else if(ar[*ptr+1] == 'N' && ar[*ptr+2] == 'E'){
+			jumpb[0]='1';
+			jumpb[1]='0';
+			jumpb[2]='1';
+		}
+		else if(ar[*ptr+1] == 'L' && ar[*ptr+2] == 'E'){
+			jumpb[0]='1';
+			jumpb[1]='1';
+			jumpb[2]='0';
+		}
+		else if(ar[*ptr+1] == 'M' && ar[*ptr+2] == 'P'){
+			jumpb[0]='1';
+			jumpb[1]='1';
+			jumpb[2]='1';
+		}
+	}
+	return jumpb;
+}
+
+void Ainstruct(char ar[],int* ptr){
+	int i;
+	instruct[0]='0';
+	instruct[16]='\0';
+	*ptr = *ptr+1;
+	int value = constant(ar,ptr);
+	for(i=15;i>0;i--){
+		instruct[i]= (value & 1) + '0';
+		value = value >> 1;
+	}
+}
+void Cinstruct(char ar[],int* ptr){
+	compb[7]='\0';
+	jumpb[3]='\0';
+	destb[3]='\0';
+
+	int i=*ptr;
+	int f1,f2;
+	int tlen = 0;
+	int j;
+	while(ar[tlen]!='\n')
+		tlen++;
+
+	while(ar[i]!= '=' && ar[i]!='\n')
+		i++;
+	if(ar[i] == '=')
+		f1 = 1;
+	else
+		f1 = 0;
+	i=*ptr;
+
+	while(ar[i]!= ';' && ar[i]!='\n')
+		i++;
+	if(ar[i] == ';')
+		f2 = 1;
+	else
+		f2 = 0;
+	if(f1 && f2){
+		i=*ptr;
+		dest(ar,&i);
+		while(ar[i] != '=')
+			i++;
+		i++;
+
+		comp(ar,&i);
+		while(ar[i] != ';')
+			i++;
+		i++;
+		jump(ar,&i);
+		*ptr=*ptr+i+2;
+	}
+	else if(f1 && !f2){
+		i=*ptr;
+		dest(ar,&i);
+		while(ar[i] != '=')
+			i++;
+		i++;
+
+		comp(ar,&i);
+		while(ar[i] != '\n')
+			i++;
+		i++;
+		*ptr=*ptr+i;
+		jumpb[0]=jumpb[1]=jumpb[2]='0';
+	}
+	else if(!f1 && f2){
+		i=*ptr;
+		comp(ar,&i);
+		while(ar[i] != ';')
+			i++;
+		i++;
+		jump(ar,&i);
+		*ptr=*ptr+i+2;
+
+		destb[0]=destb[1]=destb[2]='0';
+	}
+	instruct[0]=instruct[1]=instruct[2]='1';
+	printf("%s %ld\n",compb,strlen(compb));
+	for(i=3;i<10;i++){
+		instruct[i]=compb[i-3];
+		compb[i-10]='\0';
+	}
+	for(;i<13;i++){
+		instruct[i]=destb[i-10];
+		destb[i-10]='\0';
+	}
+	for(;i<16;i++){
+		instruct[i]=jumpb[i-13];
+		jumpb[i-10]='\0';
+	}
+
+}
+
+void whitespace(char ar[],int* ptr){
+	while(ar[*ptr]==' '||ar[*ptr]=='\t'||ar[*ptr]=='\n'){
+		*ptr = *ptr+1;
+	}
+}
+
+void comments(char ar[],int* ptr){
+	while(ar[*ptr]!='\n')
+		*ptr = *ptr+1;
+}
