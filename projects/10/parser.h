e0a70cd2d53d6ff4c3b9c462f00510c476ae556b
agrim334 <agrim334@gmail.com>
2020-09-26 23:27:52 +0530

    added class level functions, fix them

--- projects/10/parser.h
+++ projects/10/parser.h
@@ -16,12 +16,10 @@ void compileStatement();
 //class
 void compileClass();
 void compileClassVarDec();
-void compileIfstatement();
-void compileWhilestatement();
-void compileDoStatement();
-void compileReturnStatement();
-void compileLetStatement();
-void compileStatement();
+void compileVarDec();
+void compileSubroutineHead();
+void compileParameterlist();
+void compileSubroutineBody();
 
 char op(){
 	char oper = symbol();
@@ -53,6 +51,213 @@ string kwconst(){
 	cnst = "";
 	return cnst;
 }
+
+string type(){
+	string tp = keyword();
+	string primtp[] = {"int","char","boolean"};
+	for(int i = 0 ; i < 3;i++){
+		if(tp == primtp[i])
+			return primtp[i];
+	}
+	tp = identifier();
+	return tp;
+}
+
+void compileSubroutine(){
+	printf("<Subroutine>\n");
+	string nm = curtok;
+	advance();
+	string t = tokenType();
+	char s;
+	if(t == "SYMBOL"){
+		s = symbol();
+		if(s == '('){
+			printf("<subroutine identifier> %s </subroutine identifier>\n", nm.c_str());
+			printf("<symbol> %s </symbol>\n",curtok.c_str());
+			compileExplist();
+			t = tokenType();
+			if( t == "SYMBOL"){
+				s = symbol();
+				if(s == ')'){
+					printf("<symbol> %s </symbol>\n",curtok.c_str());
+					printf("</Subroutine>\n");
+				}
+			}
+		}
+		else if(s == '.'){
+			printf("<class identifier> %s </class identifier>\n", nm.c_str());
+			printf("<symbol> %s </symbol>\n",curtok.c_str());
+			advance();
+			t = tokenType();
+			if(t == "IDENTIFIER"){
+				nm = identifier();
+				printf("<subroutine identifier> %s </subroutine identifier>\n", nm.c_str());
+				advance();
+				t = tokenType();
+				if(t == "SYMBOL"){
+					s = symbol();
+					if(s == '('){
+						printf("<symbol> %s </symbol>\n",curtok.c_str());
+						compileExplist();
+						t = tokenType();
+						if( t == "SYMBOL"){
+							s = symbol();
+							if(s == ')'){
+								printf("<symbol> %s </symbol>\n",curtok.c_str());
+								printf("</Subroutine>\n");
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+
+void compileTerm_(){
+	string t;
+	t = tokenType();
+	char s = peek();
+	int i;
+	if(s == '(' || s == '.' ){
+		printf("<Term>\n");
+		compileSubroutine();
+		t = tokenType();
+		if(t == "SYMBOL" ){
+			s = symbol();
+			if(s == ']'){
+				printf("<symbol> %c </symbol>\n",s);
+				return;
+			}
+		}
+	}
+	else if(s == '['){
+		printf("<Term>\n");
+		printf("<identifier> %s </identifier>\n",curtok.c_str());
+		advance();
+		printf("<symbol> %s </symbol>\n",curtok.c_str());
+		compileExp();
+		t = tokenType();
+		if(t == "SYMBOL" ){
+			s = symbol();
+			if(s == ']'){
+				printf("<symbol> %c </symbol>\n",s);
+				return;
+			}
+		}
+	}
+	else{
+		printf("</Term>\n");
+		printf("<identifier> %s </identifier>\n",curtok.c_str());
+	}
+}
+
+
+void compileTerm(){
+	advance();
+	string t = tokenType();
+	string x;
+	int y;
+	char s;
+	if(t == "INT_CONST"){
+		y = intconstant();
+		 printf("<Term>\n");
+		 printf("<integer> %d  </integer>\n",y);
+		
+	}
+	else if( t == "STRING_CONST"){
+		x = stringconstant();
+		 printf("<Term>\n");
+		 printf("<string> %s  </string>\n",x.c_str());
+	}
+	else if( t == "KEYWORD"){
+		x = kwconst();
+		 printf("<Term>\n");
+		 printf("<keywordconst> %s </keywordconst>\n",x.c_str());
+	}
+	else if( t == "IDENTIFIER"){
+		x = identifier();
+		compileTerm_();
+	}
+	else if( t == "SYMBOL"){
+		s = symbol();
+		if(s == '('){
+
+			printf("<Term>\n");
+			printf("<symbol> %c </symbol>\n",s);
+			compileExp();
+			advance();
+			t = tokenType();
+			if (t == "SYMBOL"){
+				s = symbol();
+				if( s == ')'){
+					 printf("<symbol> %c </symbol>\n",s);
+				}
+			}
+		}
+		else{
+			s = unaryop();
+			if(s){
+				printf("<Term>\n");
+				printf("<symbol> %c </symbol>\n",s);
+				compileTerm();
+				}
+		}
+
+	}
+	 printf("</Term>\n");
+}
+
+void compileExp(){
+	printf("<Expression>\n");
+	compileTerm();
+	advance();
+	string t = tokenType();
+
+	if (t == "SYMBOL") {
+		char s = op();
+		if(s){
+			while(s){
+				printf("<symbol> %c </symbol>\n",s);
+				compileTerm();
+				advance();
+				t = tokenType();
+				if (t == "SYMBOL")
+					s = op();
+				else
+					break;
+			}
+		}
+	}
+	 printf("</Expression>\n");
+}
+
+void compileExplist(){
+	string t;
+	char s = peek();
+	if(s == ')'){
+		advance();
+		return;
+	}
+	printf("<ExpressionList>\n");
+	compileExp();
+	s = symbol();
+	if( s == ',') {
+		while(s == ',') {
+			printf("<symbol> %c </symbol>\n",s);
+			compileExp();
+//			advance();
+			t = tokenType();
+			if (t == "SYMBOL")
+				s = symbol();
+			else
+				break;
+		}
+	}
+	printf("</ExpressionList>\n");
+}
+
 void compileIfStatement(){
 	printf("<If Statement>\n");
 	advance();
@@ -184,191 +389,26 @@ void compileStatements(){
 		compileStatement();
 	}while(curtok != "}");
 }
-void compileSubroutine(){
-	printf("<Subroutine>\n");
-	string nm = curtok;
+
+void compileClassVarDec(){
+	printf("<ClassVarDeclaration>\n");
 	advance();
-	string t = tokenType();
-	char s;
-	if(t == "SYMBOL"){
-		s = symbol();
-		if(s == '('){
-			printf("<subroutine identifier> %s </subroutine identifier>\n", nm.c_str());
-			printf("<symbol> %s </symbol>\n",curtok.c_str());
-			compileExplist();
-			t = tokenType();
-			if( t == "SYMBOL"){
-				s = symbol();
-				if(s == ')'){
-					printf("<symbol> %s </symbol>\n",curtok.c_str());
-					printf("</Subroutine>\n");
-				}
-			}
-		}
-		else if(s == '.'){
-			printf("<class identifier> %s </class identifier>\n", nm.c_str());
-			printf("<symbol> %s </symbol>\n",curtok.c_str());
-			advance();
-			t = tokenType();
-			if(t == "IDENTIFIER"){
-				nm = identifier();
-				printf("<subroutine identifier> %s </subroutine identifier>\n", nm.c_str());
-				advance();
-				t = tokenType();
-				if(t == "SYMBOL"){
-					s = symbol();
-					if(s == '('){
-						printf("<symbol> %s </symbol>\n",curtok.c_str());
-						compileExplist();
-						t = tokenType();
-						if( t == "SYMBOL"){
-							s = symbol();
-							if(s == ')'){
-								printf("<symbol> %s </symbol>\n",curtok.c_str());
-								printf("</Subroutine>\n");
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-}
-
-
-void compileTerm_(){
-	string t;
-	t = tokenType();
-	char s = peek();
-	int i;
-	if(s == '(' || s == '.' ){
-		printf("<Term>\n");
-		compileSubroutine();
-		t = tokenType();
-		if(t == "SYMBOL" ){
-			s = symbol();
-			if(s == ']'){
-				printf("<symbol> %c </symbol>\n",s);
-				return;
-			}
-		}
-	}
-	else if(s == '['){
-		printf("<Term>\n");
-		printf("<identifier> %s </identifier>\n",curtok.c_str());
+	if(curtok == "static" || curtok == "field"){
+		printf("<ClassVarScope> %s </ClassVarScope>\n",curtok.c_str());
 		advance();
-		printf("<symbol> %s </symbol>\n",curtok.c_str());
-		compileExp();
-		t = tokenType();
-		if(t == "SYMBOL" ){
-			s = symbol();
-			if(s == ']'){
-				printf("<symbol> %c </symbol>\n",s);
-				return;
-			}
-		}
 	}
-	else{
-		printf("</Term>\n");
-		printf("<identifier> %s </identifier>\n",curtok.c_str());
-
-	}
-}
-
-
-void compileTerm(){
+	string t = type();
+	printf("<ClassVarType> %s </ClassVarType>\n",t.c_str());
 	advance();
-	string t = tokenType();
-	string x;
-	int y;
-	char s;
-	if(t == "INT_CONST"){
-		y = intconstant();
-		 printf("<Term>\n");
-		 printf("<integer> %d  </integer>\n",y);
-		
-	}
-	else if( t == "STRING_CONST"){
-		x = stringconstant();
-		 printf("<Term>\n");
-		 printf("<string> %s  </string>\n",x.c_str());
-	}
-	else if( t == "KEYWORD"){
-		x = kwconst();
-		 printf("<Term>\n");
-		 printf("<keywordconst> %s </keywordconst>\n",x.c_str());
-	}
-	else if( t == "IDENTIFIER"){
-		x = identifier();
-		compileTerm_();
-	}
-	else if( t == "SYMBOL"){
-		s = symbol();
-		if(s == '('){
-
-			printf("<Term>\n");
-			printf("<symbol> %c </symbol>\n",s);
-			compileExp();
-			advance();
-			t = tokenType();
-			if (t == "SYMBOL"){
-				s = symbol();
-				if( s == ')'){
-					 printf("<symbol> %c </symbol>\n",s);
-				}
-			}
-		}
-		else{
-			s = unaryop();
-			if(s){
-				printf("<Term>\n");
-				printf("<symbol> %c </symbol>\n",s);
-				compileTerm();
-				}
-		}
-
-	}
-	 printf("</Term>\n");
-}
-
-void compileExp(){
-	printf("<Expression>\n");
-	compileTerm();
+	string vn = identifier();
+	printf("<ClassVarNameDeclared> %s </ClassVarNameDeclared>\n",vn.c_str());
 	advance();
-	string t = tokenType();
-
-	if (t == "SYMBOL") {
-		char s = op();
-		if(s){
-			while(s){
-				printf("<symbol> %c </symbol>\n",s);
-				compileTerm();
-				advance();
-				t = tokenType();
-				if (t == "SYMBOL")
-					s = op();
-				else
-					break;
-			}
-		}
-	}
-	 printf("</Expression>\n");
-}
-
-void compileExplist(){
-	string t;
-	char s = peek();
-	if(s == ')'){
-		advance();
-		return;
-	}
-	printf("<ExpressionList>\n");
-	compileExp();
-	s = symbol();
+	char s = symbol();
 	if( s == ',') {
 		while(s == ',') {
 			printf("<symbol> %c </symbol>\n",s);
-			compileExp();
+			vn = identifier();
+			printf("<ClassVarNameDeclared> %s </ClassVarNameDeclared>\n",vn.c_str());
 //			advance();
 			t = tokenType();
 			if (t == "SYMBOL")
@@ -377,5 +417,140 @@ void compileExplist(){
 				break;
 		}
 	}
-	printf("</ExpressionList>\n");
+	s = symbol();
+	if (s == ';')
+		printf("<symbol> ; </symbol>\n");
+	printf("</ClassVarDeclaration>\n");
+}
+
+void compileVarDec(){
+	printf("<VarDeclaration>\n");
+	advance();
+	string t = type();
+	printf("<VarType> %s </VarType>\n",t.c_str());
+	advance();
+	string vn = identifier();
+	printf("<VarNameDeclared> %s </VarNameDeclared>\n",vn.c_str());
+	advance();
+	char s = symbol();
+	if( s == ',') {
+		while(s == ',') {
+			printf("<symbol> %c </symbol>\n",s);
+			vn = identifier();
+			printf("<VarNameDeclared> %s </VarNameDeclared>\n",vn.c_str());
+//			advance();
+			t = tokenType();
+			if (t == "SYMBOL")
+				s = symbol();
+			else
+				break;
+		}
+	}
+	s = symbol();
+	if (s == ';')
+		printf("<symbol> ; </symbol>\n");
+	printf("</VarDeclaration>\n");
+}
+
+void compileSubroutineBody(){
+	printf("<SubroutineBody>\n");
+	advance();
+	char s = symbol();
+	printf("<symbol> %c </symbol>\n",s);
+	advance();
+	while(curtok == "var"){
+		compileVarDec();
+		advance();
+	}
+	compileStatements();
+	printf("</SubroutineBody>\n");
+}
+
+void compileParameterlist(){
+	string t;
+	char s = peek();
+	if(s == ')'){
+		advance();
+		return;
+	}
+	advance();
+	printf("<ParameterList>\n");
+	t = type();
+	printf("<ParaVarType> %s </ParaVarType>\n",t.c_str());
+	advance();
+	string vn = identifier();
+	printf("<ParaVarNameDeclared> %s </ParaVarNameDeclared>\n",vn.c_str());
+	advance();
+	s = symbol();
+	if( s == ',') {
+		while(s == ',') {
+			printf("<symbol> %c </symbol>\n",s);
+			advance();
+			t = type();
+			printf("<ParaVarType> %s </ParaVarType>\n",t.c_str());
+			advance();
+			string vn = identifier();
+			printf("<ParaVarNameDeclared> %s </ParaVarNameDeclared>\n",vn.c_str());
+			advance();
+			t = tokenType();
+			if (t == "SYMBOL")
+				s = symbol();
+			else
+				break;
+		}
+	}
+	printf("</ParameterList>\n");
+}
+
+void compileSubroutineHead(){
+	advance();
+	printf("<Subroutine Declaration>\n");
+	if(curtok == "constructor" || curtok == "function" || curtok == "method"){
+		printf("<SubroutineType> %s </SubroutineType>\n",curtok.c_str());
+		advance();
+	}
+	advance();
+	string t;
+	if(curtok == "void")
+		t = curtok;
+	else
+		t = type();
+	printf("<SubroutineReturnType> %s </SubroutineReturnType>\n",t.c_str());
+	advance();
+	string sbn = identifier();
+	printf("<SubroutineName> %s </SubroutineName>\n",sbn.c_str());	
+	advance();
+	char s = symbol();
+	if(s == '('){
+		printf("<symbol> %c </symbol>\n",s);
+		compileParameterlist();
+		t = tokenType();
+		if( t == "SYMBOL"){
+			s = symbol();
+			if(s == ')'){
+				printf("<symbol> %s </symbol>\n",curtok.c_str());
+			}
+		}
+		compileSubroutineBody();
+	}
+	printf("</Subroutine Declaration>\n");
+}
+
+void compileClass(){
+	advance();
+	printf("<Class Declaration>\n");
+	string cn = identifier();
+	printf("<ClassName> %s </ClassName>\n",cn.c_str());
+	advance();
+	char s = symbol();
+	if(s == '{'){
+		printf("<symbol> %c </symbol>\n",s);
+		do{
+			compileClassVarDec();
+		}while(curtok != ";");
+		do{
+			compileSubroutineHead();
+		}while(curtok != ";");
+	}
+	printf("</Class Declaration>\n");
 }
